2920629820078a55bc153ebca7d105adda400147
司徒俊杰 <situjunjie@vip.qq.com>
2021-05-14 00:58:29 +0800

    新建注册中心

---
+++ xueyuan_ekservice/src/main/java/com/online/edu/ekservice/EkServiceApplication.java
@@ -0,0 +1,14 @@
+package com.online.edu.ekservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EkServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EkServiceApplication.class);
+    }
+}
